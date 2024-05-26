ab39e0595278b5cfc518332d58f21cbb17cee3e3
Nandini Arora <Nandiniarora338@gmail.com>
2024-05-26 12:58:58 +0530

    This is meal App

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -11,8 +11,17 @@ class MealsScreen extends StatelessWidget {
   required this.meals});
 final String title;
 final List<Meal>meals;
-void selectMeal(BuildContext context,Meal meal){
+// this selectMeal is used because  This function navigates from the current screen to a new screen that shows the details of a meal.
+
+void selectMeal(BuildContext context,Meal meal){ //BuildContext context   is a special object that gives access to the location
+  // in the widget tree where the function is called
   Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>MealDetailsScreen(meal: meal),),);
+  // A navigator manages a stack of pages (or routes) in your app, allowing you to move between different screens.
+  // push : This method adds a new route to the navigator's stack, meaning it navigates to a new screen.,
+  // MaterialPageRoute: This is a widget that creates a new route (or page) that will be animated into view. It provides the transition animation for the new screen.
+  // builder: (ctx) => MealDetailsScreen(meal: meal): This is a function that builds the new screen.
+  // (ctx): This is the context for the new route.
+ //  MealDetailsScreen(meal: meal): This means that the new screen will be an instance of MealDetailsScreen, and it will be passed the meal object as an argument.
 }
 
   @override
